712cc76e66806299ab46539eebe409f38d97fe63
ForelaxX <helloworldmkv@gmail.com>
2019-05-17 08:55:05 +0800

    add index api, return hello world

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
+const index = require('./api/index');
 const login = require('./api/login');
 const signup = require('./api/signup');
 const list = require('./api/list');
 
 module.exports = function(app) {
+  app.route("/").get(index);
   app.route("/login").post(login);
   app.route("/signup").post(signup);
   app.route("/list").get(list);
